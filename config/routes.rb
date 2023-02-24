1bd4f9eef72cd8d0c734cd1a5af5671db371183b
Ragan Hagey <raganahagey@gmail.com>
2023-02-24 11:40:24 -0500

    extra setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  Rails.application.routes.draw do
-    # route to test your configuration
-    get '/hello', to: 'application#hello_world'
-  end
-
+  # route to test your configuration
+  get '/hello', to: 'application#hello_world'
 end
